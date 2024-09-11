f75acc00d8bb967330bc0427cc3a19842061b5c5
Gustavo Lelis <gustavolelis_bh@hotmail.com>
2024-09-11 17:55:44 -0300

    pegando os valores do html

---
+++ app.js
@@ -0,0 +1,13 @@
+function sortear() {
+    let quantidade = document.getElementById('quantidade').value;
+    let de = document.getElementById('de').value;
+    let ate = document.getElementById('ate').value;
+
+    alert(`Quantidade: ${quantidade}`);
+    alert(`Do número: ${de}`);
+    alert(`Até o númeoro: ${ate}`);
+}
+
+function reiniciar() {
+
+}
